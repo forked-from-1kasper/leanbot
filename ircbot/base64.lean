e852ec9a176ce2309dce86da6eec7ae72a95ff81
1kasper <ktulhu1fhtagn@gmail.com>
2018-06-09 16:50:37 +0700

    something changes

--- ircbot/base64.lean
+++ ircbot/base64.lean
@@ -2,13 +2,7 @@ import data.bitvec data.buffer
 
 namespace base64
 
-private def base64_alphabet :=
-"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".to_list
-
-private def base64_table (id : nat) : string :=
-option.get_or_else (string.singleton <$> list.nth base64_alphabet id) ""
-
-private def group {α : Type} (n : ℕ) : list α → list (list α) :=
+def group_list {α : Type} (n : ℕ) : list α → list (list α) :=
   list.reverse ∘ list.foldl
   (λ accum (elem : α),
     match accum with
@@ -18,6 +12,12 @@ private def group {α : Type} (n : ℕ) : list α → list (list α) :=
         [elem] :: x :: xs
       else (x ++ [elem]) :: xs
     end) []
+
+private def base64_alphabet :=
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".to_list
+
+private def base64_table (id : nat) : string :=
+option.get_or_else (string.singleton <$> list.nth base64_alphabet id) ""
 
 private def padding_addition (n : ℕ) (l : list bool) : list bool :=
 list.repeat ff (n * (((l.length - 1) / n) + 1) - l.length)
@@ -32,7 +32,9 @@ private def char_to_bool_list : char → list bool :=
 padding_left 8 ∘ list.reverse ∘ nat.bits ∘ char.to_nat
 
 private def encode_bit_list : list bool → string :=
-string.join ∘ list.map (base64_table ∘ bitvec.bits_to_nat ∘ padding_right 6) ∘ group 6
+string.join ∘ list.map
+  (base64_table ∘ bitvec.bits_to_nat ∘ padding_right 6) ∘
+  group_list 6
 
 def encode (s : string) :=
 let bit_buffer := list.join $ list.map char_to_bool_list s.to_list in

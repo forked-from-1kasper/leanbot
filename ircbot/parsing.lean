529d9eeabc471022d345c286184fe4f7941f90f1
Siegmentation <siegmentationfault@yandex.ru>
2019-07-11 14:22:25 +0700

    hm, no

--- ircbot/parsing.lean
+++ ircbot/parsing.lean
@@ -108,7 +108,7 @@ PersonIdentified <|> PersonUnidentified
 def Ping : parser irc_text := do
   tok "PING",
   optional $ ch ':',
-  server ← Word, ch '\n',
+  server ← Word,
   pure $ irc_text.ping server
 
 def NormalMessage : parser irc_text := do
